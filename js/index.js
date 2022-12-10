0a6a3ffce1a4ee14de413fe14b863309ddf9a015
dwivedyaakash <aakashdwd07@gmail.com>
2022-12-10 16:44:39 +0530

    update: formatting and comments

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
-$('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
+// Navbar Scroll
+$(".js-scroll-trigger").click(function () {
+  $(".navbar-collapse").collapse("hide");
 });
