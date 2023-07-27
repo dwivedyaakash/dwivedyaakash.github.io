650ce4f8d55141fb178f6c87040f3c57d1bb2928
dwivedyaakash <aakashdwd07@gmail.com>
2023-07-27 21:54:21 +0530

    update: icons

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,11 +2,7 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {
-      boxShadow: {
-        DEFAULT: 'rgba(149, 157, 165, 0.2) 0px 8px 24px;',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
